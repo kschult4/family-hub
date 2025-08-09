e43b3e795e40c2cfc681cf21a55d8be6e11335ca
Kyle Schultz <kyle.e.schultz@gmail.com>
2025-08-08 22:53:39 -0400

    Fix GitHub Pages deployment with correct base path

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/family-hub/sw.js', { scope: '/family-hub/' })})}
