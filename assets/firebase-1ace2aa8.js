f8a29e59eb0cdf08e7b2266217ad9c630f8b9954
Kyle Schultz <kyle.e.schultz@gmail.com>
2025-08-08 22:58:15 -0400

    Fix production errors: add graceful fallbacks for Firebase and external APIs

---
+++ assets/firebase-1ace2aa8.js
@@ -0,0 +1,16 @@
+import{r as a,i as e,g as s}from"./index.esm-6ea0700f.js";var r="firebase",i="12.1.0";/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */a(r,i,"app");const t={apiKey:"AIzaSyAgfhlVrNGwhdux6qbmMuSx4wotxMy0pGU",authDomain:"family-hub-dashboard.firebaseapp.com",databaseURL:"https://family-hub-dashboard-default-rtdb.firebaseio.com",projectId:"family-hub-dashboard",storageBucket:"family-hub-dashboard.firebasestorage.app",messagingSenderId:"237122444853",appId:"1:237122444853:web:8d2836b0aea34422d0db7c"},b=e(t),o=s(b);export{o as database,b as default};
